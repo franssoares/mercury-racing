33b529dd5ecc3e159a45519d42820bcdd60a7dc6
franssoares <xflsn100@gmail.com>
2026-05-27 18:21:59 -0300

    feat(router): configure routes and navigation nesting

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,33 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Layout } from "./components/Layout";
+import { Home } from "./pages/Home";
+import { Standings } from "./pages/Standings";
+import { Calendar } from "./pages/Calendar";
+import { Login } from "./pages/Login";
+import { NotFound } from "./pages/NotFound";
+import { Realtime } from "./pages/Realtime";
+import { DriverProfile } from "./pages/DriverProfile";
+import { H2H } from "./pages/H2H";
+import { AppDashboard } from "./pages/AppDashboard";
+
 function App() {
     return (
-        <div>
-            <h1>Mercury Racing - Em construção</h1>
-        </div>
+        <BrowserRouter>
+            <Layout>
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/standings" element={<Standings />} />
+                    <Route path="/calendar" element={<Calendar />} />
+                    <Route path="/drivers" element={<DriverProfile />} />
+                    <Route path="/login" element={<Login />} />
+                    <Route path="/app" element={<AppDashboard />}>
+                        <Route path="h2h" element={<H2H />} />
+                        <Route path="realtime" element={<Realtime />} />
+                    </Route>
+                    <Route path="*" element={<NotFound />} />
+                </Routes>
+            </Layout>
+        </BrowserRouter>
     );
 }
 
